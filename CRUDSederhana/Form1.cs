93bb11618efe3223b705c06ff005c98a3bc8be1f
RMadya <kereyoben75@gmail.com>
2025-03-21 17:02:47 +0700

    kemudian bagian untuk memuat data dari database dan menampilkannya di form.  kemudian bagian untuk menghapus isi dari beberapa kontrol input pada form

--- CRUDSederhana/Form1.cs
+++ CRUDSederhana/Form1.cs
@@ -16,4 +16,27 @@ namespace CRUDSederhana
         // Ganti "SERVER" sesuai dengan SQL Server Anda
         private string connectionString = "Data Source=NALz\\MUHAMMADBAGAS;Initial Catalog=OrganisaniMahasiswa;Integrated Security=True";
 
-        
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        // Event saat form pertama kali dimuat
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            LoadData();
+        }
+
+        // Fungsi untuk mengosongkan semua input pada TextBox
+        private void ClearForm()
+        {
+            txtNIM.Clear();
+            txtNama.Clear();
+            txtEmail.Clear();
+            txtTelepon.Clear();
+            txtAlamat.Clear();
+            txtNIM.Focus();  // Fokus kembali ke NIM agar user siap memasukkan data baru
+        }
+
+        // Fungsi untuk menampilkan data di DataGridView
+       
